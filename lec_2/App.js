cb6354d82b6e273f78152b807398a603ccd31f07
Supreet <supreet7111999@gmail.com>
2025-06-23 21:54:06 +0530

    Supreet|Brief about react render

---
+++ lec_2/App.js
@@ -0,0 +1,9 @@
+
+const head=React.createElement("h1",
+{id:"heading",xyz:"abc"},
+"Hello King from React !");
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(head);
+
+
+//in createElement fun , 2nd arg are props or attributes
